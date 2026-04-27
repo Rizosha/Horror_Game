d05b7f16ef350eaa134532341b42978ec85be70c
Adam Mir <adam.mir2003@outlook.com>
2026-04-27 23:42:01 +0100

    Hotbar and Inventory improvements
    Hotbar that now displays the item icon and amount
    Inventory can now handle obtaining multiple stacks of items and correctly adding them to the hotbar / inventory

--- Assets/Scripts/BookInteractable.cs
+++ Assets/Scripts/BookInteractable.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class BookInteractable : MonoBehaviour, Interfaces.IInteractable
 {
+    [SerializeField] private Inventory.InventoryItem item;
+    
     public void Interact()
     {
         Debug.Log("Interaction happened");
+        Inventory.Instance.AddItemToInventory(item, item.quantity);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : Singleton<Inventory>
 {
@@ -24,7 +27,9 @@ public class Inventory : Singleton<Inventory>
     
     public List<InventoryItem> playerInventory = new List<InventoryItem>();
     [SerializeField] private int inventorySlots = 6; // How many slots in the inventory
-
+    [SerializeField] private List<Image> playerHotbarImages = new();
+    [SerializeField] private List<TMP_Text> playerHotbarTexts = new();
+    [SerializeField] private GameObject hotbar;
     
     #region Testing
     public bool addItemToInventoryTest;
@@ -49,8 +54,42 @@ public class Inventory : Singleton<Inventory>
         }
     }
     #endregion
+    
+    private void Start()
+    {
+        playerHotbarTexts = new List<TMP_Text>(hotbar.GetComponentsInChildren<TMP_Text>());
+        
+        // Find all hotbar images via tag and then add them to an array after sorting them in order via hierarchy from the parent
+        var hotbarImages = GameObject.FindGameObjectsWithTag("HotbarImage")
+            .OrderBy(obj => obj.transform.parent.GetSiblingIndex())
+            .ToArray();
+        
+        foreach (var image in hotbarImages)
+        {
+            playerHotbarImages.Add(image.GetComponent<Image>());
+        }
 
+        if (playerHotbarImages != null)
+        {
+            foreach (var img in playerHotbarImages)
+            {
+                img.color = new Color(1, 1, 1, 0);
+            }
+        }
 
+        if (playerHotbarTexts != null)
+        {
+            foreach (var text in playerHotbarTexts)
+            {
+                text.color = new Color(1, 1, 1, 0);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Player hot bar text list is null");
+        }
+    }
+    
     public void AddItemToInventory(InventoryItem item, int quantity)
     {
         if (playerInventory.Count >= inventorySlots)
@@ -65,16 +104,57 @@ public class Inventory : Singleton<Inventory>
             if (item.itemData.stackable)
             {
                 item.quantity = quantity;
-                playerInventory.Add(item);
+
+                if (item.quantity >= item.itemData.maxStack)
+                {
+                    float amountToAdd = item.quantity;
+                    
+                    // Add one stack
+                    item.quantity = item.itemData.maxStack;
+                    playerInventory.Add(item);
+
+                    amountToAdd -= item.itemData.maxStack;
+                    Debug.LogError(amountToAdd);
+                    var stacksToAdd = amountToAdd /  item.itemData.maxStack;
+                    var stacksToAddRoundedUp = Mathf.Ceil(stacksToAdd);
+                    Debug.LogError(stacksToAddRoundedUp);
+
+                    for (var i = 0; i < stacksToAddRoundedUp; i++)
+                    {
+                        if (stacksToAddRoundedUp > 1)
+                        {
+                            stacksToAddRoundedUp--;
+                            amountToAdd -= item.itemData.maxStack;
+                            playerInventory.Add(item);
+                        }
+                        if (stacksToAddRoundedUp == 1) // Sort out the final not full stack
+                        {
+                            item.quantity = (int)amountToAdd;
+                            Debug.LogError($"Final stack {amountToAdd}");
+                            playerInventory.Add(item);
+                        }
+                    }
+                }
             }
+            
             else
             {
                 for (var i = 0; i < quantity; i++)
                 {
+                    if (playerInventory.Count >= inventorySlots)
+                    {
+                        Debug.Log("Possible overflow items not added to inventory");
+                        // TODO: Sort this out
+                        // - Item is left on ground with pickup quantity reduced?
+                        break;
+                    }
+                    
                     playerInventory.Add(item);
                 }      
             }
         }
+        
+        UpdateInventoryIcons();
     }
 
     public void RemoveItemFromInventory(InventoryItem item, int quantity)
@@ -102,6 +182,38 @@ public class Inventory : Singleton<Inventory>
             {
                 item.quantity -= quantity;
                 Debug.Log("Inventory item quantity decreased");
+            }
+        }
+        
+        UpdateInventoryIcons();
+    }
+    
+    private void UpdateInventoryIcons()
+    {
+        var sprites = new List<Sprite>();
+        sprites.Clear();
+        
+        foreach (var inventoryItem in playerInventory)
+        {
+            var itemIcon = inventoryItem.itemData.icon;
+            sprites.Add(itemIcon);
+        }
+
+        for (var i = 0; i < inventorySlots; i++)
+        {
+            try
+            {
+                playerHotbarImages[i].sprite = sprites[i];
+                playerHotbarImages[i].color = new Color(1, 1, 1, 1);
+                
+                playerHotbarTexts[i].text = playerInventory[i].quantity.ToString();
+                playerHotbarTexts[i].color = new Color(1, 1, 1, 1);
+            }
+            catch
+            {
+                playerHotbarImages[i].color = new Color(1, 1, 1, 0);
+                playerHotbarTexts[i].color = new Color(1, 1, 1, 0);
+                Debug.Log("player hot bar image could not be filled - likely due to the inventory containing less items than slots");
             }
         }
     }
